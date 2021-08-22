2a1ce3c9629a9a65cab0202632fa0e55e6cea860
Rune Petersen <58623004+runembp@users.noreply.github.com>
2021-08-22 17:49:42 +0000

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    stringVar = input("Give a string:")
+    intVar = input("Give an integer:")
+    floatVar = input("Give a float:")
+    boolVar = input("Give a boolean:")
+
+    print("You gave the string " + stringVar)
+    print("You gave the integer " + str(intVar))
+    print("You gave the float " + str(floatVar))
+    print("You gave the boolean " + str(boolVar))
 
 if __name__ == '__main__':
     main()
